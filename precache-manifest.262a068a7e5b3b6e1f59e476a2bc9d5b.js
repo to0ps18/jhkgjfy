b6278258cb9b3cd93bd1a99b631085ee7aad3337
ertovkax <s280906k@gmail.com>
2021-11-30 07:54:10 +0300

    Updates

---
+++ precache-manifest.262a068a7e5b3b6e1f59e476a2bc9d5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60005975ae75232a15ef836d5282b9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/js/2.bcb1d5ed.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.bcb1d5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "./static/js/main.c9fee35b.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
